72c1c5eac55fb405423e6e41ec67a23c6309e19d
derricklewis <derrickjameslewis@gmail.com>
2020-05-25 08:22:40 -0400

    remove OCV type check

--- spark/spark_image.py
+++ spark/spark_image.py
@@ -3,46 +3,12 @@ import numpy as np
 import os
 import pandas as pd
 import cv2
-# from PIL import Image
-# from io import BytesIO
-from collections import namedtuple, Counter
+from collections import Counter
 from sklearn.cluster import KMeans
-# from pyspark.mllib.clustering import KMeans, KMeansModel
 from pyspark.sql.functions import col, pandas_udf, udf, struct, PandasUDFType, split
 from pyspark.sql.types import *
 
-# From Databricks ImageIO source code:
-_OcvType = namedtuple("OcvType", ["name", "ord", "nChannels", "dtype"])
-
-_SUPPORTED_OCV_TYPES = (
-    _OcvType(name="CV_8UC1", ord=0, nChannels=1, dtype="uint8"),
-    _OcvType(name="CV_32FC1", ord=5, nChannels=1, dtype="float32"),
-    _OcvType(name="CV_8UC3", ord=16, nChannels=3, dtype="uint8"),
-    _OcvType(name="CV_32FC3", ord=21, nChannels=3, dtype="float32"),
-    _OcvType(name="CV_8UC4", ord=24, nChannels=4, dtype="uint8"),
-    _OcvType(name="CV_32FC4", ord=29, nChannels=4, dtype="float32"),
-)
-# NOTE: Build dict of all types from above
-_OCV_TYPES_BY_ORDINAL = {m.ord: m for m in _SUPPORTED_OCV_TYPES}
-
-
-def imageTypeByOrdinal(ordinal):
-    """
-    NOTE: From DataBrick Source code (becuase it was impossible to get installed on AWS.)
-
-    Connects the Mode value returned by spark.read.format('image')... into the OCV type above to be converted.
-    PARAMS: ordinal value from mode int 0 - 29
-
-    returns:  named tuple with the following -
-       1. OpenCV image type
-       2. the original mode value
-       3. the number of color channels in the image (nChannels)
-       4. the data type each pixel value is store in
-    """
-    if not ordinal in _OCV_TYPES_BY_ORDINAL:
-        raise KeyError("unsupported image type with ordinal %d, supported OpenCV types = %s" % (
-            ordinal, str(_SUPPORTED_OCV_TYPES)))
-    return _OCV_TYPES_BY_ORDINAL[ordinal]
+IMAGES_PATH = 's3a://social-system-test/instagram_graph_image_store/*/'
 
 
 def color_many_classify(image, shape):
@@ -70,9 +36,8 @@ def color_many_classify(image, shape):
     dom_color_5 = clt.cluster_centers_[label_counts.most_common(5)[4][0]]
     dom_color_6 = clt.cluster_centers_[label_counts.most_common(6)[5][0]]
 
-    # create a square showing dominant color of equal size to input image for testing
+    # create RGB pixel values from cluster centers
     dom_color_1_hsv = np.full(shape, dom_color_1, dtype='uint8')
-    # convert to bgr color space for display in testing
     dom_color_1_rgb = cv2.cvtColor(dom_color_1_hsv, cv2.COLOR_HSV2RGB)
 
     dom_color_2_hsv = np.full(shape, dom_color_2, dtype='uint8')
@@ -161,6 +126,18 @@ attributes = [
 
 @pandas_udf(StructType(attributes), PandasUDFType.GROUPED_MAP)
 def convert_imageStruct(key, pdf):
+    """
+    Converts image Byte Array from Spark into a 3d vector, concats 
+    all images of the pands dataframe in the group and classifies the 
+    6 color centers of all images combined.
+
+    PARAMS: 
+    key : value that was used to groupby in Pands UDF Grouped_Map
+    pdf : resulting pandas DF of grouping
+
+    RETURNS: 
+    Single row Pandas DF with key and string of dominant color values.
+    """
     bgr_image = []
     for index, row in pdf.iterrows():
         if row['mode'] == 16:
@@ -170,20 +147,20 @@ def convert_imageStruct(key, pdf):
             dtype = imageTypeByOrdinal(row['mode']).dtype
             image = np.ndarray(shape, dtype, row['data'])
             image = image[..., ::-1]
-            image = cv2.resize(image, (150, 150), interpolation=cv2.INTER_AREA)
+            image = cv2.resize(image, (75, 75), interpolation=cv2.INTER_AREA)
             if len(bgr_image) == 0:
                 bgr_image = image
             else:
                 bgr_image = np.concatenate((bgr_image, image), axis=0)
         else:
             pass
-    shape = bgr_image.shape
-    # convert to HSV; this is a better representation of how we see color
-    hsv_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV)
-    image = hsv_image.reshape((hsv_image.shape[0] * hsv_image.shape[1], 3))
-    if len(image) > 1:
+
+    try:
+        shape = bgr_image.shape
+        hsv_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV)
+        image = hsv_image.reshape((hsv_image.shape[0] * hsv_image.shape[1], 3))
         colors = color_many_classify(image, shape)
-    else:
+    except:
         colors = ""
     return pd.DataFrame([[key[0], str(colors)]], columns=['igId', 'colors'])
 
@@ -195,15 +172,15 @@ if __name__ == "__main__":
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0")\
         .getOrCreate()
 
-    IMAGES_PATH = 's3a://social-system-test/instagram_graph_image_store/*/*.jpg'
-
-    df = spark.read.format("image").option(
-        "dropInvalid", True).load(IMAGES_PATH)
-
     # Settings to allow pyarrow to work with Pandas UDF.
     spark.conf.set("spark.sql.execution.arrow.enabled", "true")
     # Set a large batch size in practice.
     spark.conf.set("spark.sql.execution.arrow.maxRecordsPerBatch", "6400")
+    # Pass over corrupt images
+    spark.conf.set("spark.sql.files.ignoreCorruptFiles", "true")
+
+    df = spark.read.format("image").option(
+        "dropInvalid", True).load(IMAGES_PATH)
 
     # Flatten the Struct in the sprak df
     image_batch = df.select('image.origin',
@@ -213,13 +190,16 @@ if __name__ == "__main__":
                             'image.nChannels',
                             'image.data')
 
-    # Get The Instagram user ID from the file structure
+    # Get The Instagram user ID from the folder in the image path
     image_batch = image_batch.withColumn('igId', split('origin', "/")[4])
 
-    # Run the big conversion. Take Binary Array of image, convert it to nd.Array of pixels and KMeans on the colors.
+    # First attempt to limit shuffling
+    image_batch.repartition('igId')
+
+    # Run the main conversion. Take Binary Array of image, convert it to nd.Array of pixels and KMeans on the colors.
     final = image_batch.groupby('igId').apply(convert_imageStruct)
 
-    # beta.show(1)
+    # TODO Using repartition again here or coalesce might limit shuffling but force the work to a single node.
     final.write.mode("overwrite").save(
         "s3a://social-system-test/spark/output/")
 
