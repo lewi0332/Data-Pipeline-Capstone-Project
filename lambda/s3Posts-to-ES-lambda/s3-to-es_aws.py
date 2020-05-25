b0a0f0e551ba8d80941301146c81da408b631ce9
derricklewis <derrickjameslewis@gmail.com>
2020-05-25 12:47:25 -0400

    initial lambda upload

---
+++ lambda/s3Posts-to-ES-lambda/s3-to-es_aws.py
@@ -0,0 +1,194 @@
+import boto3
+import json
+import datetime
+import urllib
+import urllib3
+import logging
+from pprint import pprint
+from requests_aws4auth import AWS4Auth
+import requests
+import botocore
+from io import BytesIO
+import re
+
+
+"""
+Can Override the global variables using Lambda Environment Parameters
+"""
+globalVars = {}
+globalVars['Owner'] = "Derrick"
+globalVars['Environment'] = "Dev"
+globalVars['awsRegion'] = "us-east-1"
+globalVars['tagName'] = "serverless-s3-to-es-log-ingester"
+globalVars['service'] = "es"
+globalVars['esIndexPrefix'] = "/instagram_graph_posts/"
+globalVars['esIndexDocType'] = "_doc"
+globalVars['esHosts'] = {
+    'prod': 'something more secure here',
+    'aws': 'https://search-social-system-kkehzvprsvgkfisnfulapobkpm.us-east-1.es.amazonaws.com'
+}
+
+# Initialize Logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def indexDocElement(es_Url, awsauth, docData):
+    try:
+        headers = {"Content-Type": "application/json"}
+        # headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        resp = requests.put(es_Url, auth=awsauth,
+                            headers=headers, json=docData)
+        print(resp.content)
+        if resp.status_code == 201:
+            logger.info('INFO: Successfully created element into ES')
+        elif resp.status_code == 200:
+            logger.info('INFO: Successfully updated element into ES')
+        else:
+            logger.error(f'FAILURE: Unable to index element {resp.content}')
+            raise
+    except Exception as e:
+        logger.error(f'ERROR: {str(e)}')
+        logger.error(f"ERROR: Unable to index line:{docData['content']}")
+        raise
+
+
+def store_images(temp, bucket):
+    """
+    Stores images from media url to S3 bucket. 
+    Checks for image in S3, load it if absent
+    Checks for children Media urls and saves as such: 
+    17841401753941377 / 10010784827803388 / original.jpg, 17870925466585765.jpg, 18015701020271224.jpg
+    """
+
+    s3 = boto3.client('s3')
+
+    if 'media_url' in temp.keys():
+        image = temp['media_url']
+    elif 'thumbnail_url' in temp.keys():
+        image = temp['thumbnail_url']
+    else:
+        return 'image load fail'
+
+    img_key = 'instagram_graph_image_store/' + \
+        str(temp['owner_id']) + '/' + str(temp['id'])
+
+    # Get file extension (.jpg or .mp4)
+    ext = re.search(r'\.\w{3,4}(?=\?)', image).group()
+
+    # look for photos already in bucket
+    response = s3.list_objects_v2(
+        Bucket=bucket,
+        Prefix=img_key,
+    )
+    # if key count 0, there is no photo already stored in s3
+    if response.get('KeyCount', []) < 1:
+        response = requests.get(image).content
+        s3.put_object(Bucket=bucket, Key=img_key +
+                      '/original' + ext, Body=response)
+        logger.info(f"Added {img_key + '/original.jpg'} to S3")
+
+        # if this is a carousel post, get all child images
+        if 'children' in temp.keys():
+            for i in range(len(temp['children']['data'])):
+                img_id = temp['children']['data'][i]['id']
+                image = temp['children']['data'][i]['media_url']
+                ext = re.search(r'\.\w{3,4}(?=\?)', image).group()
+                tmp_key = img_key + '/' + img_id + ext
+                response = requests.get(image).content
+                s3.put_object(Bucket=bucket, Key=tmp_key, Body=response)
+        # if this is a video get the thumbnail
+        elif 'thumbnail_url' in temp.keys():
+            img_id = 'thumbnail'
+            image = temp['thumbnail_url']
+            ext = re.search(r'\.\w{3,4}(?=\?)', image).group()
+            tmp_key = img_key + '/' + img_id + ext
+            response = requests.get(image).content
+            s3.put_object(Bucket=bucket, Key=tmp_key, Body=response)
+        else:
+            logger.info('No carousel images found')
+    else:
+        logger.info(
+            f"Found the file {img_key + '/original.jpg'} already in S3")
+    return 'https://social-system-test.s3.amazonaws.com/' + img_key + '/original' + ext
+
+
+def lambda_handler(event, context):
+    credentials = boto3.Session().get_credentials()
+
+    # Set up connection to S3 bucket with raw json
+    s3 = boto3.client('s3')
+
+    # set up connection to AWS Elasticsearch
+    awsauth = AWS4Auth(credentials.access_key,
+                       credentials.secret_key,
+                       globalVars['awsRegion'],
+                       globalVars['service'],
+                       session_token=credentials.token
+                       )
+    logger.info("Received event: " + json.dumps(event, indent=2))
+
+    try:
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        key = urllib.parse.unquote_plus(
+            event['Records'][0]['s3']['object']['key'])
+
+        # Get document (obj) form S3
+        obj = s3.get_object(Bucket=bucket, Key=key)
+
+    except Exception as e:
+        logger.error('ERROR: {0}'.format(str(e)))
+        logger.error(
+            'ERROR: Unable able to GET object:{0} from S3 Bucket:{1}. Verify object exists.'.format(key, bucket))
+
+    # .replace("'", "") #.replace("\\n", " ")
+    body = obj['Body'].read().decode("utf-8")
+    logger.info('SUCCESS: Retreived object from S3')
+
+    # Create document headers
+    docData = {}
+    docData['objectKey'] = str(key)
+    docData['createdDate'] = str(obj['LastModified'])
+    docData['content_type'] = str(obj['ContentType'])
+    docData['content_length'] = str(obj['ContentLength'])
+
+    # parsing content before sending to Elasticsearch
+    temp = json.loads(body)
+
+    # items not needed in ES.
+    temp.pop("ig_id", None)
+    temp.pop("username", None)
+    temp.pop("is_comment_enabled", None)
+
+    # flatten Owner ID
+    temp['owner_id'] = temp['owner']['id']
+
+    # set post id to string
+    temp['id'] = str(temp['id'])
+
+    # look for followers, if none set to -1
+    try:
+        temp['followers'] = temp['owner']['followers_count']
+    except Exception as e:
+        print(f'Failed to set followers. {e}')
+        temp['followers'] = -1
+    temp.pop('owner')
+
+    # load images to S3
+    temp['fohr_media'] = store_images(temp, bucket)
+
+    # package up the document data
+    docData['content'] = temp
+
+    # Build Elasticsearch URL
+    es_Url = globalVars['esHosts'].get(
+        'aws') + globalVars['esIndexPrefix'] + globalVars['esIndexDocType'] + '/' + temp['id']
+
+    # send to AWS ES
+    logger.info('Calling AWS ES...')
+    indexDocElement(es_Url, awsauth, docData)
+    logger.info('SUCCESS: Successfully indexed the entire doc into ES')
+
+
+if __name__ == '__main__':
+    lambda_handler(None, None)
---
+++ lambda/s3Stats-to-ES-lambda/s3Stats/statsApp.py
@@ -0,0 +1,140 @@
+import boto3
+import json
+import datetime
+import urllib
+import urllib3
+import logging
+from pprint import pprint
+from requests_aws4auth import AWS4Auth
+import requests
+import botocore
+from io import BytesIO
+import re
+
+
+"""
+Can Override the global variables using Lambda Environment Parameters
+"""
+globalVars = {}
+globalVars['Owner'] = "Derrick"
+globalVars['Environment'] = "Dev"
+globalVars['awsRegion'] = "us-east-1"
+globalVars['tagName'] = "serverless-s3-to-es-stat-ingester"
+globalVars['service'] = "es"
+globalVars['esIndexPrefix'] = "/instagram_graph_users/"
+globalVars['esIndexDocType'] = "_doc"
+globalVars['esHosts'] = {
+    'prod': 'future ES store',
+    'aws': 'https://search-social-system-kkehzvprsvgkfisnfulapobkpm.us-east-1.es.amazonaws.com'
+}
+
+# Initialize Logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def indexDocElement(es_Url, awsauth, docData):
+    try:
+        headers = {"Content-Type": "application/json"}
+        # headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        resp = requests.put(es_Url, auth=awsauth,
+                            headers=headers, json=docData)
+        print(resp.content)
+        if resp.status_code == 201:
+            logger.info('INFO: Successfully created element into ES')
+        elif resp.status_code == 200:
+            logger.info('INFO: Successfully updated element into ES')
+        else:
+            logger.error(f'FAILURE: Unable to index element {resp.content}')
+            raise
+    except Exception as e:
+        logger.error(f'ERROR: {str(e)}')
+        logger.error(f"ERROR: Unable to index line:{docData['content']}")
+        raise
+
+
+def lambda_handler(event, context):
+    credentials = boto3.Session().get_credentials()
+
+    # Set up connection to S3 bucket with raw json
+    s3 = boto3.client('s3')
+
+    # set up connection to AWS Elasticsearch
+    awsauth = AWS4Auth(credentials.access_key,
+                       credentials.secret_key,
+                       globalVars['awsRegion'],
+                       globalVars['service'],
+                       session_token=credentials.token
+                       )
+    logger.info("Received event: " + json.dumps(event, indent=2))
+
+    try:
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        key = urllib.parse.unquote_plus(
+            event['Records'][0]['s3']['object']['key'])
+
+        # Get document (obj) form S3
+        obj = s3.get_object(Bucket=bucket, Key=key)
+
+    except Exception as e:
+        logger.error('ERROR: {0}'.format(str(e)))
+        logger.error(
+            'ERROR: Unable able to GET object:{0} from S3 Bucket:{1}. Verify object exists.'.format(key, bucket))
+
+    # .replace("'", "") #.replace("\\n", " ")
+    body = obj['Body'].read().decode("utf-8")
+    logger.info('SUCCESS: Retreived object from S3')
+
+    # Create document headers
+    docData = {}
+    docData['objectKey'] = str(key)
+    docData['createdDate'] = str(obj['LastModified'])
+    docData['content_type'] = str(obj['ContentType'])
+    docData['content_length'] = str(obj['ContentLength'])
+
+    # parsing content before sending to Elasticsearch
+    temp = json.loads(body)
+
+    # load images to S3
+    image = temp['profile_picture_url']
+    ext = re.search(r'\.\w{3,4}(?=\?)', image).group()
+    img_key = 'instagram_graph_image_store/' + \
+        str(temp['id']) + '/' + "profile" + ext
+
+    response = s3.list_objects_v2(
+        Bucket=bucket,
+        Prefix=img_key,
+    )
+    if response.get('KeyCount', []) < 1:
+        response = requests.get(image).content
+        s3.put_object(Bucket=bucket, Key=img_key, Body=response)
+        logger.info(f"Added {img_key + '/profile.jpg'} to S3")
+    else:
+        logger.info(
+            f"Found the file {img_key} already in S3")
+
+    temp.pop("ig_id", None)
+    temp.pop("email_contacts", None)
+    temp.pop("phone_call_clicks", None)
+    temp.pop("text_message_clicks", None)
+    temp.pop("get_directions_clicks", None)
+    temp.pop("website_clicks", None)
+    temp.pop("profile_views", None)
+
+    temp['profile_picture'] = 'https://social-system-test.s3.amazonaws.com/' + img_key
+
+    # package up the document data
+    docData['content'] = temp
+
+    # Build Elasticsearch URL
+    es_Url = globalVars['esHosts'].get(
+        'aws') + globalVars['esIndexPrefix'] + globalVars['esIndexDocType'] + '/' + str(temp['id'])
+
+    # send to AWS ES
+    logger.info('Calling AWS ES...')
+    indexDocElement(es_Url, awsauth, docData)
+    logger.info('SUCCESS: Successfully indexed the entire doc into ES')
+
+
+if __name__ == '__main__':
+    lambda_handler(None, None)
